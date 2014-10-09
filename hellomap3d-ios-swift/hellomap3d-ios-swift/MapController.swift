526ca9e79ee465fc04ed0c0512fb680f4828759a
oooits72 <oooits72@gmail.com>
2014-10-09 16:05:55 +0600

    add simple popup

--- hellomap3d-ios-swift/hellomap3d-ios-swift/MapController.swift
+++ hellomap3d-ios-swift/hellomap3d-ios-swift/MapController.swift
@@ -18,6 +18,25 @@ class MapController: NTMapViewController {
         // Initialize a raster layer with the data source, and add it to the MapView
         let rasterLayer = NTRasterTileLayer(dataSource: rasterdDataSource)
         getLayers().add(rasterLayer)
+        
+        let mapProj = getOptions().getBaseProjection()
+        
+        let vectorDataSource = NTUnculledVectorDataSource(projection: mapProj)
+        
+        let pos = mapProj.fromWgs84(NTMapPos(x: 24.651488, y: 59.423581))
+        
+        let ballonStyleBuilder = NTBalloonPopupStyleBuilder()
+        ballonStyleBuilder.setPlacementPriority(10)
+        
+        let testPopUp = NTBalloonPopup(pos: pos, style: ballonStyleBuilder.buildStyle(), title: "en: test", desc: "ru: тест")
+        vectorDataSource.add(testPopUp)
+        
+        let vectorLayer = NTVectorLayer(dataSource: vectorDataSource)
+        
+        getLayers().add(vectorLayer)
+        
+        setZoom(14, durationSeconds: 0.0)
+        setFocusPos(pos, durationSeconds: 0.0)
     }
 }
 
