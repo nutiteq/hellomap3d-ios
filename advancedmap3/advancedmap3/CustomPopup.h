2dd0149f6641debbf08403e082c35bac5e3e1b9e
Mark Tehver <mark.tehver@gmail.com>
2015-01-20 23:00:04 +0200

    Customized popup sample

---
+++ advancedmap3/advancedmap3/CustomPopup.h
@@ -0,0 +1,13 @@
+//
+//  CustomPopup.h
+//  advancedmap3
+//
+//  Created by Mark Tehver on 20/01/15.
+//  Copyright (c) 2015 Nutiteq. All rights reserved.
+//
+
+#ifndef advancedmap3_CustomPopup_h
+#define advancedmap3_CustomPopup_h
+
+
+#endif
---
+++ advancedmap3/advancedmap3/CustomPopupSampleController.mm
@@ -0,0 +1,9 @@
+//
+//  CustomPopupSampleController.m
+//  advancedmap3
+//
+//  Created by Mark Tehver on 20/01/15.
+//  Copyright (c) 2015 Nutiteq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
